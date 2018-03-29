9f1e8f42a243af6267f7510d8066da6c86318f2f
the Tim Ridenour <theTimRidenour@gmail.com>
2018-03-28 22:42:52 -0500

    added search ver 1

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,21 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobsResult;
+
+        if(searchType.equals("all")) {
+            jobsResult = JobData.findByValue(searchTerm);
+        } else {
+            jobsResult = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobsResult);
+        return "search";
+
+    }
 
 }
